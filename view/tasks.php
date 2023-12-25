f420559a009585cf6bdb26d89e855a6ad2b7ae54
Douae sb <douae.sb411@gmail.com>
2023-12-25 10:50:17 +0100

    isloggedin

--- view/tasks.php
+++ view/tasks.php
@@ -1,3 +1,10 @@
+<?php
+require_once('../controller/usercontroller.php');
+
+$user = new usercontroller();
+$user->isLoggedIn();
+$user->logout();
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -81,11 +88,13 @@
                     </div>
                 </div>
                 <div class="p-4">
-                    <button type="button" class="inline-flex items-center justify-center h-9 px-4 rounded-xl bg-gray-900 text-gray-300 hover:text-white text-sm font-semibold transition">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" class="" viewBox="0 0 16 16">
-                            <path d="M12 1a1 1 0 0 1 1 1v13h1.5a.5.5 0 0 1 0 1h-13a.5.5 0 0 1 0-1H3V2a1 1 0 0 1 1-1h8zm-2 9a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
-                        </svg>
-                    </button> <span class="font-bold text-sm ml-2">Logout</span>
+                <a class="font-bold text-sm ml-2" href="dashboard.php?deconn">
+        <button type="button" class="inline-flex items-center justify-center h-9 px-4 rounded-xl bg-gray-900 text-gray-300 hover:text-white text-sm font-semibold transition">
+          <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" class="" viewBox="0 0 16 16">
+            <path d="M12 1a1 1 0 0 1 1 1v13h1.5a.5.5 0 0 1 0 1h-13a.5.5 0 0 1 0-1H3V2a1 1 0 0 1 1-1h8zm-2 9a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
+          </svg>
+        </button>
+        </a> <a class="font-bold text-sm ml-2" href="dashboard.php?deconn">Logout</a>
                 </div>
             </div>
         </aside>
