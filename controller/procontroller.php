40dd75184fae72a00c2be27ff8bfda49b9f593b0
Douae sb <douae.sb411@gmail.com>
2023-12-26 14:40:17 +0100

    update radia

--- controller/procontroller.php
+++ controller/procontroller.php
@@ -1,41 +1,51 @@
 <?php
 require_once('../model/projectModel.php');
 
-class procontroller{
+class procontroller
+{
 
-public function DisplayProjects(){
-    $iduser = $_SESSION['iduser'];
-    $projet = new ProjectModel();
-    return $projet->displayProject($iduser);
-}
-
-public function AddProjects(){
-        
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['addpro'])) {
-    $projet = new ProjectModel();
-    $projet->setNompro($_POST['nompro']);
-    $projet->setDescpro($_POST['descpro']);
-    $iduser = $_SESSION['iduser'];
-    $projet->addProject($iduser);
-    header('Location: projects.php');
-    exit;
-}
+    public function DisplayProjects()
+    {
+        $iduser = $_SESSION['iduser'];
+        $projet = new ProjectModel();
+        return $projet->displayProject($iduser);
     }
 
-    public function EditProjects(){
-        
+    public function AddProjects()
+    {
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['addpro'])) {
+            $projet = new ProjectModel();
+            $projet->setNompro($_POST['nompro']);
+            $projet->setDescpro($_POST['descpro']);
+            $iduser = $_SESSION['iduser'];
+            $projet->addProject($iduser);
+            header('Location: projects.php');
+            exit;
+        }
     }
-    
+
+    public function EditProjects()
+    {
+        $project = new ProjectModel();
+        if (isset($_POST['editprojet']) && isset($_POST['idpro'])) {
+            $idpro = $_POST['idpro'];
+            $project->setNompro($_POST['nompro']);
+            $project->setDescpro($_POST['descpro']);
+            $project->editProject($idpro);
+            header("Location: projects.php");
+            exit();
+        }
+    }
+
     public function deleteProject()
     {
-        if (isset($_GET['idpro'])) {
+        if (isset($_GET['deletepro']) && isset($_GET['idpro']) ) {
             $idpro = $_GET['idpro'];
-            $projet =new ProjectModel();
+            $projet = new ProjectModel();
             $projet->deleteProject($idpro);
             header('Location: projects.php');
             exit();
         }
     }
-
 }
-?>
